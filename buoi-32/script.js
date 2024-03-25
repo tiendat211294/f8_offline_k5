a1bfa657339da0e67cbe953f96fc974e6015da1b
tiendat <tiendat211294@gmail.com>
2024-03-25 10:42:04 +0700

    Bai tap buoi 32

--- buoi-32/script.js
+++ buoi-32/script.js
@@ -2,22 +2,34 @@ var list = document.querySelector(".list");
 var listItems = list.querySelectorAll(".list-item");
 
 for (let i = 0; i < listItems.length; i++) {
-  var offsetY = 0;
-  var newNode = listItems[i];
-  listItems[i].addEventListener("dragstart", function () {
-    listItems[i].classList.add("ghost");
-    newNode.classList.add("ghost");
-    document.addEventListener("mousemove", function (e) {
-      offsetY = e.offsetY;
-    });
-    console.log(offsetY, listItems[i].offsetTop);
-    if (offsetY > listItems[i].offsetTop) {
-      list.insertBefore(listItems[i], newNode);
-    } else if (offsetY < listItems[i].offsetTop) {
-      list.insertBefore(listItems[i + 1], newNode);
-    }
+  var currentElement;
+  var dragElement;
+  listItems[i].addEventListener("dragstart", function (e) {
+    e.currentTarget.classList.add("ghost");
+    dragElement = e.currentTarget;
+    currentElement = e.currentTarget.getBoundingClientRect();
   });
   listItems[i].addEventListener("dragend", function (e) {
-    listItems[i].classList.remove("ghost");
+    e.currentTarget.classList.remove("ghost");
+    var lessonCount = 0;
+    var chapterCount = 0;
+    listItems = list.querySelectorAll(".list-item");
+    listItems.forEach(function (item) {
+      if (item.classList.contains("active")) {
+        chapterCount++;
+        item.childNodes[1].childNodes[0].innerText = `Chương ${chapterCount}:`;
+      } else {
+        lessonCount++;
+        item.childNodes[1].childNodes[0].innerText = `Bài ${lessonCount}:`;
+      }
+    });
+  });
+  listItems[i].addEventListener("dragover", function (e) {
+    var tempElement = e.currentTarget.getBoundingClientRect();
+    if (currentElement.top > tempElement.top + tempElement.height / 2) {
+      list.insertBefore(dragElement, e.currentTarget);
+    } else if (currentElement.bottom < tempElement.bottom) {
+      list.insertBefore(dragElement, e.currentTarget.nextElementSibling);
+    }
   });
 }
