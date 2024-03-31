14101024d909dcf41740d35f2678a08e8351abc9
tiendat <tiendat211294@gmail.com>
2024-03-31 11:31:39 +0700

    bai tap buoi 34

---
+++ buoi-34/script.js
@@ -0,0 +1,21 @@
+var counter = document.querySelector(".counter");
+var getLinkBtn = document.querySelector(".get-link-btn");
+let start;
+function step(timeStamp) {
+  if (start === undefined) {
+    start = timeStamp;
+  }
+  const elapsed = timeStamp - start;
+  const count = Math.floor(elapsed / 1000);
+  counter.innerText = 30 - count;
+
+  if (elapsed <= 30000) {
+    window.requestAnimationFrame(step);
+  } else {
+    getLinkBtn.disabled = "";
+    getLinkBtn.addEventListener("click", function () {
+      window.location.href = "https://fullstack.edu.vn/";
+    });
+  }
+}
+window.requestAnimationFrame(step);
