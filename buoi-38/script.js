8059cbb0b2b2dee393ec66ac8360dbc3e46c6881
tiendat <tiendat211294@gmail.com>
2024-04-14 18:36:44 +0700

    bai tap buoi 38

---
+++ buoi-38/script.js
@@ -0,0 +1,25 @@
+const postApi = `http://localhost:3000/posts`;
+const container = document.querySelector(".container");
+
+const showPost = async () => {
+  const response = await fetch(postApi);
+  const postData = await response.json();
+  let html = ``;
+  postData.forEach((post) => {
+    html += `<h3>${post.title}</h3>
+    <p>${post.text}</p>`;
+  });
+  const postHtml = document.createElement("div");
+  postHtml.innerHTML = html;
+  container.append(postHtml);
+};
+showPost();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight - 1
+  ) {
+    showPost();
+  }
+});
